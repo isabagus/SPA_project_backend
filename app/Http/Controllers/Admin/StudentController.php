5c3d762c7eef8346b17b973d11b59f9d80938d32
SamuelDwiS <samdwisaputro02@gmail.com>
2026-05-01 16:26:02 +0700

    "update routes"

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -20,21 +20,6 @@ class StudentController extends Controller
     public function create()
     {
         return view('layouts.students.create');
-<<<<<<< HEAD
-=======
-    }
-    public function update()
-    {
-
-    }
-    public function edit()
-    {
-
-    }
-    public function delete()
-    {
-
->>>>>>> 54c2d1b8e3839cac0a07e8feb8d33836ea605ce2
     }
     public function update() {}
     public function edit() {}
